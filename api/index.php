802803eee6efae6ea59efb1f03974e1bc5b19aed
Braulio Rojas <brauliojrojas@gmail.com>
2020-11-29 12:26:51 -0400

    fix

--- api/index.php
+++ api/index.php
@@ -1,2 +1,4 @@
-<?php// Reenviar las solicitudes de Vercel a index.php normalmente
-require __DIR__ . '/public/index.php';
+<?php
+
+// Reenviar las solicitudes de Vercel a index.php normalmente
+require __DIR__ . '/../public/index.php';
