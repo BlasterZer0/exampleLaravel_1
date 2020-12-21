db3b8c431afa1d0a79381040484ab74d74e4ebe7
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-21 10:36:18 -0400

    Auth required

--- app/Http/Controllers/StudUpdateController.php
+++ app/Http/Controllers/StudUpdateController.php
@@ -5,6 +5,17 @@ use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 class StudUpdateController extends Controller {
+    
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
 public function index(){
 $users = DB::select('select * from users');
 return view('stud_edit_view',['users'=>$users]);
