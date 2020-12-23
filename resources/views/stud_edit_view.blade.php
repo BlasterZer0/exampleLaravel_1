c2a1bd8d5c6183c8c1b13a6ab2831b6e33a320e9
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-23 12:54:18 -0400

    Update stud_edit_view.blade.php

--- resources/views/stud_edit_view.blade.php
+++ resources/views/stud_edit_view.blade.php
@@ -41,8 +41,114 @@
 </table>
 </div>
 <div>
-<link href=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css rel=stylesheet>
-<link href=https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css rel=stylesheet>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500" rel="stylesheet"/>
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
+<script>
+$(document).ready(function() {
+   $("#sort").DataTable({
+      columnDefs : [
+    { type : 'date', targets : [3] }
+],  
+   });
+});
+</script>
+<style>
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+
+/* Zebra striping */
+
+tr:nth-of-type(odd) {
+  background: #f4f4f4;
+}
+
+tr:nth-of-type(even) {
+  background: #fff;
+}
+
+th {
+  background: #782f40;
+  color: #ffffff;
+  font-weight: 300;
+}
+
+td,
+th {
+  padding: 10px;
+  border: 1px solid #ccc;
+  text-align: left;
+}
+
+td:nth-of-type(1) {
+  font-weight: 500 !important;
+}
+
+td {
+  font-family: 'Roboto', sans-serif !important;
+  font-weight: 300;
+  line-height: 20px;
+}
+
+span {
+  font-style: italic
+}
+
+@media only screen and (max-width: 760px),
+(min-device-width: 768px) and (max-device-width: 1024px) {
+
+  /* Force table to not be like tables anymore */
+  table.responsive,
+  .responsive thead,
+  .responsive tbody,
+  .responsive th,
+  .responsive td,
+  .responsive tr {
+    display: block !important;
+  }
+
+  /* Hide table headers (but not display: none;, for accessibility) */
+  .responsive thead tr {
+    position: absolute !important;
+    top: -9999px;
+    left: -9999px;
+  }
+
+  .responsive tr {
+    border: 1px solid #ccc;
+  }
+
+  .responsive td {
+    /* Behave  like a "row" */
+    border: none;
+    border-bottom: 1px solid #eee !important;
+    position: relative !important;
+    padding-left: 25% !important;
+  }
+
+  .responsive td:before {
+    /* Now like a table header */
+    position: absolute !important;
+    /* Top/left values mimic padding */
+    top: 6px;
+    left: 6px;
+    width: 45%;
+    padding-right: 10px;
+    white-space: nowrap !important;
+    font-weight: 500 !important;
+  }
+
+  /*
+	Label the data
+	*/
+  .responsive td:before {
+    content: attr(data-table-header) !important;
+  }
+}
+</style>
 <div class="row">
 <div class="container"> 
   
@@ -74,11 +180,14 @@
         </tbody>    
 @endforeach
 </table>
-<script src=https://code.jquery.com/jquery-3.5.1.js></script>
-<script src=https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js></script>
-<script src=https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js></script>
+</div>
 </div>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/dataTables.bootstrap.min.js"></script>
+<script src "https://cdn.datatables.net/plug-ins/1.10.15/sorting/stringMonthYear.js"></script>
+</div>
                 
             </div>
         </div>
