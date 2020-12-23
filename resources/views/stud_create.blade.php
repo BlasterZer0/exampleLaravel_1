9b31b5e5ad7e7bd5fd361959577c414d80d4cd5c
Braulio Rojas <brauliojrojas@gmail.com>
2020-12-23 03:46:58 -0400

    b

--- resources/views/stud_create.blade.php
+++ resources/views/stud_create.blade.php
@@ -4,29 +4,43 @@
    </head>
 
    <body>
+   @extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">{{ __('Añadir Usuario') }}</div>
       <form action = "/create" method = "post">
          <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
          <table>
             <tr>
-               <td>Name</td>
+               <td>Nombre</td>
                <td><input type='text' name='name' /></td>
             </tr>
             <tr>
-               <td>Email</td>
+               <td>Correo Electrónico</td>
                <td><input type='text' name='email' /></td>
             </tr>
             <tr>
-               <td>Password</td>
+               <td>Contraseña</td>
                <td><input type='text' name='password' /></td>
             </tr>
             <tr>
                <td colspan = '2'>
-                  <input type = 'submit' value = "Add student"/>
+                  <input type = 'submit' value = "Añadir Usuario"/>
                   <a href = "/edit-records">Volver</a>
                </td>
             </tr>
          </table>
       </form>
       
+      </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection      
    </body>
 </html>
--- resources/views/stud_update.blade.php
+++ resources/views/stud_update.blade.php
@@ -4,26 +4,35 @@
 <title>Student Management | Edit</title>
 </head>
 <body>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">{{ __('Editar Usuario') }}</div>
+                <div>
 <div>
 <form action = "/edit/<?php echo $users[0]->id; ?>" method = "post">
 <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
 <table>
 <tr>
-<td>Name</td>
+<td>Nombre</td>
 <td>
 <input type = 'text' name = 'name'
 value = '<?php echo$users[0]->name; ?>'/>
 </td>
 </tr>
 <tr>
-<td>Email</td>
+<td>Correo Electrónico</td>
 <td>
 <input type = 'text' name = 'email'
 value = '<?php echo$users[0]->email; ?>'/>
 </td>
 </tr>
 <tr>
-<td>Password</td>
+<td>Contraseña</td>
 <td>
 <input type = 'text' name = 'password'
 value = '<?php echo$users[0]->password; ?>'/>
@@ -32,12 +41,18 @@ value = '<?php echo$users[0]->password; ?>'/>
 <tr>
 <td colspan = '2'>
 <input type = 'submit' value = "Update User" />
-<a href = "/edit-records">Volver</a>
+<button type = "button"><a href = "/edit-records">Volver</a></button>
 </td>
 </tr>
 </table>
 </form>
 </div>
 
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
 </body>
 </html> 
