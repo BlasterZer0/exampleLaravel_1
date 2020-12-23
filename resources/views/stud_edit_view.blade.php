ab7c0fb7006b953950c4fb28bb5df753de4ce5e9
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-23 13:29:08 -0400

    Update stud_edit_view.blade.php

--- resources/views/stud_edit_view.blade.php
+++ resources/views/stud_edit_view.blade.php
@@ -79,7 +79,7 @@ tr:nth-child(even) {
     <td>{{ $user->name }}</td>
     <td>{{ $user->email }}</td>
     <td><a href = 'edit/{{ $user->id }}' class="btn btn-warning">Editar</a></td>
-    <td><a href = 'delete/{{ $user->id }}' class="btn btn-danger">Borrar</a></td>
+    <!--<td><a href = 'delete/{{ $user->id }}' class="btn btn-danger">Borrar</a></td>-->
     <td><a class="btn btn-danger" onclick="return confirm('Are you sure?')" href='delete/{{ $user->id }}'><i class="fa fa-trash"></i></a></td>
   </tr>
 @endforeach
