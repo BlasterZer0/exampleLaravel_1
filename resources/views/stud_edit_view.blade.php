37a353625abea92cb15355d1a47867b61039c803
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-23 13:05:28 -0400

    Update stud_edit_view.blade.php

--- resources/views/stud_edit_view.blade.php
+++ resources/views/stud_edit_view.blade.php
@@ -2,6 +2,26 @@
 <html>
 <head>
 <title>Tabla de usuarios</title>
+<style>
+table {
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
+
+th {
+  cursor: pointer;
+}
+
+th, td {
+  text-align: left;
+  padding: 16px;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2
+}
+</style>
 </head>
 <body>
 @extends('layouts.app')
@@ -40,153 +60,108 @@
 @endforeach
 </table>
 </div>
+
 <div>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,500" rel="stylesheet"/>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
-<script>
-$(document).ready(function() {
-   $("#sort").DataTable({
-      columnDefs : [
-    { type : 'date', targets : [3] }
-],  
-   });
-});
-</script>
-<style>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
+<p><strong>Click the headers to sort the table.</strong></p>
+<p>The first time you click, the sorting direction is ascending (A to Z).</p>
+<p>Click again, and the sorting direction will be descending (Z to A):</p>
 
-
-/* Zebra striping */
-
-tr:nth-of-type(odd) {
-  background: #f4f4f4;
-}
-
-tr:nth-of-type(even) {
-  background: #fff;
-}
-
-th {
-  background: #782f40;
-  color: #ffffff;
-  font-weight: 300;
-}
-
-td,
-th {
-  padding: 10px;
-  border: 1px solid #ccc;
-  text-align: left;
-}
-
-td:nth-of-type(1) {
-  font-weight: 500 !important;
-}
-
-td {
-  font-family: 'Roboto', sans-serif !important;
-  font-weight: 300;
-  line-height: 20px;
-}
-
-span {
-  font-style: italic
-}
-
-@media only screen and (max-width: 760px),
-(min-device-width: 768px) and (max-device-width: 1024px) {
-
-  /* Force table to not be like tables anymore */
-  table.responsive,
-  .responsive thead,
-  .responsive tbody,
-  .responsive th,
-  .responsive td,
-  .responsive tr {
-    display: block !important;
-  }
-
-  /* Hide table headers (but not display: none;, for accessibility) */
-  .responsive thead tr {
-    position: absolute !important;
-    top: -9999px;
-    left: -9999px;
-  }
-
-  .responsive tr {
-    border: 1px solid #ccc;
-  }
-
-  .responsive td {
-    /* Behave  like a "row" */
-    border: none;
-    border-bottom: 1px solid #eee !important;
-    position: relative !important;
-    padding-left: 25% !important;
-  }
-
-  .responsive td:before {
-    /* Now like a table header */
-    position: absolute !important;
-    /* Top/left values mimic padding */
-    top: 6px;
-    left: 6px;
-    width: 45%;
-    padding-right: 10px;
-    white-space: nowrap !important;
-    font-weight: 500 !important;
-  }
-
-  /*
-	Label the data
-	*/
-  .responsive td:before {
-    content: attr(data-table-header) !important;
-  }
-}
-</style>
-<div class="row">
-<div class="container"> 
-  
-  <h1>Bootstrap 3 SortTable</h1> 
-  <table class="table responsive" id="sort">
-	<thead>
-		<tr>
-			<th scope="col">ID</th>
-			<th scope="col">Name</th>
-			<th scope="col">Email</th>
-			<th scope="col">Edit</th>
-			<th scope="col">Delete</th>
-		</tr>
-	</thead>
-@foreach ($users as $user)
-        <tbody>
-            <tr>
-            <td data-table-header="ID">{{ $user->id }}</td>
-			<td data-table-header="Name">{{ $user->name }}</td>
-			<td data-table-header="Email">{{ $user->email }}</td>
-			<td data-table-header="Edit"><a href = 'edit/{{ $user->id }}' class="btn btn-warning">Editar</a></td>
-			<td data-table-header="Delete"><a href = 'delete/{{ $user->id }}' class="btn btn-danger">Borrar</a></td>
-        <!--        <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->email }}</td>
-                <td><a href = 'edit/{{ $user->id }}' class="btn btn-warning">Editar</a></td>
-                <td><a href = 'delete/{{ $user->id }}' class="btn btn-danger">Borrar</a></td>-->
-            </tr>
-        </tbody>    
-@endforeach
+<table id="myTable">
+  <tr>
+   <!--When a header is clicked, run the sortTable function, with a parameter, 0 for sorting by names, 1 for sorting by country:-->  
+    <th onclick="sortTable(0)">Name</th>
+    <th onclick="sortTable(1)">Country</th>
+  </tr>
+  <tr>
+    <td>Berglunds snabbkop</td>
+    <td>Sweden</td>
+  </tr>
+  <tr>
+    <td>North/South</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Koniglich Essen</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>Paris specialites</td>
+    <td>France</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Canada</td>
+  </tr>
 </table>
-</div>
-</div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/jquery.dataTables.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/js/dataTables.bootstrap.min.js"></script>
-<script src "https://cdn.datatables.net/plug-ins/1.10.15/sorting/stringMonthYear.js"></script>
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
 </div>
                 
             </div>
