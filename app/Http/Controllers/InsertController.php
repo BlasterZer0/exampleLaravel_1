1eb2d46f02ee7c24380f121d7c229d13ceaddc05
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-28 09:57:55 -0400

    Update InsertController.php

--- app/Http/Controllers/InsertController.php
+++ app/Http/Controllers/InsertController.php
@@ -24,6 +24,22 @@ class InsertController extends Controller {
    public function insertform() {
       return view('create_db');
    }
+    
+     /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+    
    public function insert(Request $request) {
       $name = $request->input('name');
       $email = $request->input('email');
