5d581642b15f88ce12e41c88b0c73f864892d5a0
Braulio Rojas <35616590+BlasterZer0@users.noreply.github.com>
2020-12-28 14:28:55 -0400

    Update edit_db.blade.php

--- resources/views/edit_db.blade.php
+++ resources/views/edit_db.blade.php
@@ -81,7 +81,7 @@ function sortTable(n) {
   table = document.getElementById("myTable");
   switching = true;
   //Set the sorting direction to ascending:
-  dir = "des"; 
+  dir = "asc"; 
   /*Make a loop that will continue until
   no switching has been done:*/
   while (switching) {
